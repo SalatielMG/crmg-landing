a9b1e6473732422d90c640200d08a62471c67a76
Salatiel Montero Gonzalez <salatiel.monteroglez@gmail.com>
2023-12-17 00:52:52 -0600

    refactor(environment.test): add a new environment by test

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,6 @@
+import { EnvironmentInitInterface } from "src/app/core/environment/environment.interface";
+
+export const environment: EnvironmentInitInterface = {
+  production: true,
+  urlFetch: 'assets/env/environment.test.json'
+};
